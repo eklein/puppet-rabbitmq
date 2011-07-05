1d8ee1d5abd0ea68cf20c60684b3cbf4c47359fb
narkisr <narkisr@gmail.com>
2011-07-05 18:12:02 +0300

    patching init file to include pid file creation

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,6 +3,12 @@
 #
 class rabbitmq::service {
 
+      # this patches init file to create a pid see http://tinyurl.com/6y2wkqq  
+      file{"/etc/init.d/rabbitmq-server":
+	   source => "puppet://puppet/modules/rabbitmq/rabbitmq-server",
+	   require	=> Yum::Localinstall["rabbitmq-server"],
+	   notify => Service["rabbitmq-server"]
+      }
 
 	service { "rabbitmq-server":
 		ensure    => running,
